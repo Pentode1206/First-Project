08e2e63410d176a2b316749e229855a2fc5ea0e2
Jim Tiner <james.c.tiner@outlook.com>
2022-05-08 09:07:30 -0400

    Added some code

--- Hello.py
+++ Hello.py
@@ -1,1 +1,10 @@
+#The obligitory Hello program
+#08MAY22
 print('Hello')
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return num * factorial(num -1)
+    
+print (factorial(9))
