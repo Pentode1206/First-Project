8fdad145a715aad9f9290c8aed3a477190ca1dd5
Jim Tiner <james.c.tiner@outlook.com>
2022-05-08 21:50:50 -0400

    New Python stuff

---
+++ Strings.py
@@ -0,0 +1,10 @@
+# The following example is from the Python Tutorial
+# Found at https://docs.python.org/3.7/tutorial/introduction.html
+# It demostrates use of the tripple quote """ and the escape character \ to produce formatted
+# console output.
+
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
